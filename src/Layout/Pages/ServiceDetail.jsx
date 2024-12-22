a9c88c46dbb83fc6e9276990052dab4fccc3e96b
Sanjida <sanjida.ice.778@gmail.com>
2024-12-23 02:59:03 +0600

    all services are shown in all services page

--- src/Layout/Pages/ServiceDetail.jsx
+++ src/Layout/Pages/ServiceDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ServiceCard from '../../Components/ServiceCard';
 
 const ServiceDetail = () => {
+
     return (
         <div>
             {/* <ServiceCard></ServiceCard> */}
